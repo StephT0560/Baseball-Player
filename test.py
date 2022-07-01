7297ef58a3166e231034bf43c8df9b98bed181fc
Stephen Tisselin <stisselin216@gmail.com>
2022-07-01 16:57:23 +0000

    Sixth

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import unittest
-from baseball import function1, function2
+from baseball import getList, filterDict
 
 
 class TestFileName(unittest.TestCase):
@@ -7,7 +7,7 @@ class TestFileName(unittest.TestCase):
         self.assertEqual(getList({"brand": "Ford","model": "Mustang","year": 1964}), ['brand', 'model', 'year'])
 
     def test_function2(self):
-        self.assertEqual(createAPIData("http://api.open-notify.org/astros.json"), '{"number": 10, "people": [{"name": "Oleg Artemyev", "craft": "ISS"}, {"name": "Denis Matveev", "craft": "ISS"}, {"name": "Sergey Korsakov", "craft": "ISS"}, {"name": "Kjell Lindgren", "craft": "ISS"}, {"name": "Bob Hines", "craft": "ISS"}, {"name": "Samantha Cristoforetti", "craft": "ISS"}, {"name": "Jessica Watkins", "craft": "ISS"}, {"name": "Cai Xuzhe", "craft": "Tiangong"}, {"name": "Chen Dong", "craft": "Tiangong"}, {"name": "Liu Yang", "craft": "Tiangong"}], "message": "success"}')
-
+        self.assertEqual(filterDict({"brand": "Ford","model": "Mustang","year": 1964},"brand"),{"brand": "Ford"})
+        
 if __name__ == '__main__':
     unittest.main()
