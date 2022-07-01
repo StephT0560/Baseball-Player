0e311e7f9fd0f23ef534f56cac4ffd2f17303eae
Stephen Tisselin <stisselin216@gmail.com>
2022-07-01 16:48:43 +0000

    Fifth commit

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
 import unittest
-from yourCodeFileName import function1, function2
+from baseball import function1, function2
 
 
 class TestFileName(unittest.TestCase):
     def test_function1(self):
-        self.assertEqual(function1(1), 0)
+        self.assertEqual(getList({"brand": "Ford","model": "Mustang","year": 1964}), ['brand', 'model', 'year'])
 
     def test_function2(self):
-        self.assertEqual(function2(2,1), 3)
-        self.assertEqual(function2(2.1, 1.2), 3.3)
+        self.assertEqual(createAPIData("http://api.open-notify.org/astros.json"), '{"number": 10, "people": [{"name": "Oleg Artemyev", "craft": "ISS"}, {"name": "Denis Matveev", "craft": "ISS"}, {"name": "Sergey Korsakov", "craft": "ISS"}, {"name": "Kjell Lindgren", "craft": "ISS"}, {"name": "Bob Hines", "craft": "ISS"}, {"name": "Samantha Cristoforetti", "craft": "ISS"}, {"name": "Jessica Watkins", "craft": "ISS"}, {"name": "Cai Xuzhe", "craft": "Tiangong"}, {"name": "Chen Dong", "craft": "Tiangong"}, {"name": "Liu Yang", "craft": "Tiangong"}], "message": "success"}')
 
 if __name__ == '__main__':
     unittest.main()
